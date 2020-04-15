640f0fab75a3560589425c844d34bb7ce792afad
Sanjay S <srikumar.sanjay@gmail.com>
2020-04-14 23:07:36 -0400

    Initial commit

---
+++ personal.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+  var particles = Particles.init({
+    selector: '.background',
+    color: ['#FFFFFF', '#000000', '#eeeeee'],
+    speed: 1,
+    connectParticles: true,
+    maxParticles: 250,
+  });
+};
